87df61b49a15289e818cb54ed053ea5a0ee20517
thelittlebassist <59612912+thelittlebassist@users.noreply.github.com>
2020-01-29 14:37:47 +0100

    Create delete_supplier.php

---
+++ delete_supplier.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<title>Dostawca został usunięty</title>
+			<?php include('core/bootstrap.php');?>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<?php
+				include('core/config.php');
+
+				$sql = "DELETE From Dostawca WHERE Id_supplier = :Id_supplier";
+				$data = ['Id_supplier' => $Id_supplier
+				];
+			
+				if ($stmt = $pdo->prepare($sql)) {
+                	if ($stmt->execute(array(':Id_supplier'=>trim($_GET['Id_supplier'])))) {
+                  }
+                }
+
+
+				try {
+		            $pdo->prepare($sql)->execute($data);
+		            echo '<div class="alert alert-success">';
+		            echo "Dostawca został usunięty <br> <a class='btn btn-outline-dark btn-sm'  href='http://localhost/Dostawcy_lista.php'>Lista dostawców</a>";
+		            echo '</div>';
+		            die ("");
+        	}
+		        catch(Exception $e) {
+		            echo '<div class="alert alert-warning">';
+		            echo 'Exception -> ';
+		            echo ($e->getMessage());
+		            die("<br>Coś poszło nie tak z usuwaniem  <br> <a class='btn btn-outline-dark btn-sm'  href='http://localhost/Dostawcy_lista.php'>Lista dostawców</a></div>");
+		        	}
+			?>
+		</div>
+	</body>
+</html>
