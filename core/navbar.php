d798e3e9705d517f46a40b1e3ad5c3fb9f958846
Robert Rychlicki <thelittlebassist@gmail.com>
2020-07-07 10:02:06 +0200

    Drobne poprawki (ignore, navbar)

--- core/navbar.php
+++ core/navbar.php
@@ -19,6 +19,9 @@
           <li class="nav-item active">
             <a class="nav-link" href="http://localhost/EKOCHEM-URSU/List_faktury.php">Faktury<span class="sr-only">(current)</span></a>
           </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="http://localhost/EKOCHEM-URSU/List_persons.php">Pracownicy<span class="sr-only">(current)</span></a>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="https://trello.com/b/MH3jpE68/ekochem-30">Trello</a>
           </li>
